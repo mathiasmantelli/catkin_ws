249c717f092411f2b13ec364689d35496af16c68
Mathias Mantelli <mathiasfassini@gmail.com>
2020-08-17 14:13:41 -0300

    Turtlebot moving and printing its pose in Python.

--- src/ros_essentials_cpp/src/assignment/robot_move_topics.py
+++ src/ros_essentials_cpp/src/assignment/robot_move_topics.py
@@ -2,18 +2,30 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+import time
+from std_srvs.srv import Empty
 
-def move_robot():
-    mvrb_publisher = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
-    rospy.init_node('robot_move_topics', anonymous=True)
+def callback(pose_message):
+    global x
+    global y, z, yaw
+    x= pose_message.x
+    y= pose_message.y
+    yaw = pose_message.theta
+
+    print "pose callback"
+    print ('x = {}'.format(pose_message.x)) #new in python 3
+    print ('y = %f' %pose_message.y) #used in python 2
+    print ('yaw = {}'.format(pose_message.theta)) #new in python 3
+
+def move_robot(speed, angle):
+       
     rate = rospy.Rate(10) # 10hz
 
     myTwist = Twist()
-    myTwist.linear.y = 1.0
-    myTwist.linear.x = 1.0
-    myTwist.angular.z = 1.0
+    myTwist.linear.x = speed
+    myTwist.angular.z = angle
     
-
     while not rospy.is_shutdown():
 
         mvrb_publisher.publish(myTwist)
@@ -21,6 +33,21 @@ def move_robot():
 
 if __name__ == '__main__':
     try:
-        move_robot()
+        rospy.init_node('robot_move_topics', anonymous=True)
+
+        mvrb_publisher = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
+        rospy.Subscriber('turtle1/pose', Pose, callback)
+        time.sleep(2)
+
+        print 'move: '
+        move_robot(0.5, -0.5)
+        time.sleep(2)
+        print 'start reset: '
+        rospy.wait_for_service('reset')
+        reset_turtle = rospy.ServiceProxy('reset', Empty)
+        reset_turtle()
+        print 'end reset: '
+        rospy.spin()
+       
     except rospy.ROSInterruptException:
-        pass
+        rospy.loginfo("node terminated.")
