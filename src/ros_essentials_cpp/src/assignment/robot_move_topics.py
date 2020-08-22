030702997e8fbf9f8607632756b41c2415edc9bd
Mathias Mantelli <mathiasfassini@gmail.com>
2020-08-22 11:36:30 -0300

    Turtlebot randomly moving and printing its pose in Python.

--- src/ros_essentials_cpp/src/assignment/robot_move_topics.py
+++ src/ros_essentials_cpp/src/assignment/robot_move_topics.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 import time
 from std_srvs.srv import Empty
+import numpy as np
 
 def callback(pose_message):
     global x
@@ -18,16 +19,15 @@ def callback(pose_message):
     print ('y = %f' %pose_message.y) #used in python 2
     print ('yaw = {}'.format(pose_message.theta)) #new in python 3
 
-def move_robot(speed, angle):
+def move_robot():
        
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(1) # 10hz
 
     myTwist = Twist()
-    myTwist.linear.x = speed
-    myTwist.angular.z = angle
     
     while not rospy.is_shutdown():
-
+        myTwist.linear.x = np.random.uniform(0,1) #randomly moving the robot forward
+        myTwist.angular.z = np.random.uniform(-2.5,2.5) #randomly turning the robot   
         mvrb_publisher.publish(myTwist)
         rate.sleep()
 
@@ -40,7 +40,7 @@ if __name__ == '__main__':
         time.sleep(2)
 
         print 'move: '
-        move_robot(0.5, -0.5)
+        move_robot()
         time.sleep(2)
         print 'start reset: '
         rospy.wait_for_service('reset')
